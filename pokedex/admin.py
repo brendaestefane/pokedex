00a3ae1d900d8a7216ac8437b2aa457b16570b82
Brenda <bsouza.436@gmail.com>
2019-05-02 22:07:04 -0300

    Parei nesse momento de programar

---
+++ pokedex/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+admin.site.register(Habilidade)
+admin.site.register(Categoria)
+admin.site.register(Pokemon)
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\TI-Turma-B\pokedex\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
